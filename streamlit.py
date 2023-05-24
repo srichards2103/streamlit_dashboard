64e66df110478b179bb31cd938b86c7e04af77ad
u7104627 <u7104627@anu.edu.au>
2023-05-25 09:13:41 +1000

    fdas

--- streamlit.py
+++ streamlit.py
@@ -86,7 +86,7 @@ col3.pyplot(figure)
 st.header("Total Trades Placed")
 
 col_x = st.columns(1)
-col_x.metric = ("Total Trades Placed", trades_p.shape[0], None)
+col_x.metric = ("Total Trades Placed", len(trades_p), None)
 col4, col5, col6 = st.columns(3)
 
 ## Add EV for all orders placed within past 24 hours from now
